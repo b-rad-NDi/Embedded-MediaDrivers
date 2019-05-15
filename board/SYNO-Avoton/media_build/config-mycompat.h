a93b5c81c55a3af03be4447a458f4cd3c306724c
Brad Love <brad@nextdimension.cc>
2019-05-15 16:18:41 -0500

    Add profile for Synology Avoton devices
    Intel Atom C2538
    - DS1515+
    - DS1815+
    - DS2415+
    - DS415+
    - RS815+/RS815RP+
    
    Signed-off-by: Brad Love <brad@nextdimension.cc>

---
+++ board/SYNO-Avoton/media_build/config-mycompat.h
@@ -0,0 +1,9 @@
+#undef NEED_PM_RUNTIME_GET
+
+#define replace_fops(f, fops) \
+	do {	\
+		struct file *__file = (f); \
+		fops_put(__file->f_op); \
+		BUG_ON(!(__file->f_op = (fops))); \
+	} while(0);
+
